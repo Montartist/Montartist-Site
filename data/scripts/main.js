60d1673c1e1fd5ccdd5267e35baa9c5a842022ec
MorskiDev <103139159+MorskiDev@users.noreply.github.com>
2025-05-16 18:55:43 +0200

    Continuing header menu

--- data/scripts/main.js
+++ data/scripts/main.js
@@ -1,10 +1,16 @@
 var menuConcours = document.getElementById('concours')
 var menuPortfolio = document.getElementById('portfolio')
+var body = document.getElementsByTagName('body')[0]
 
-menuConcours.addEventListener("mouseover", function (){
-    menuConcours.innerHTML = 'CONCOURS<div id="subConcours" class="subMenu"><ul class="subMenu"><li class="subItem"><a href="Concours/Participants">Participants</a></li><li class="subItem"><a href="Concours/Selectionnes">Sélectionnés</a></li></ul></div>'
+menuConcours.addEventListener("focus", function (){
+    menuConcours.innerHTML = 'CONCOURS <div id="subConcours" class="subMenu"><ul class="subMenu"><li class="subItem"><a id="subMenuParticipants" href="/Concours/Participants">Participants</a></li><li class="subItem"><a id="subMenuSelectionnes" href="Concours/Selectionnes">Sélectionnés</a></li></ul></div>'
+})
+body.addEventListener("click", function (e,menuConcours){
+    if (e.clientX < document.getElementById('subConcours').getBoundingClientRect.left && menuConcours.innerHTML != 'CONCOURS') {
+        menuConcours.innerHTML = 'CONCOURS'
+    }
+    console.log(e.clientX)
+    console.log(document.getElementById('subConcours').getBoundingClientRect.left)
+    
 })
 
-menuConcours.addEventListener("mouseout", function (){
-    menuConcours.innerHTML = 'CONCOURS'
-})
