9af741ba6ff8538522498f2d7b7c19f8ca14274e
IILilGuy <lilguydums@gmail.com>
2025-09-23 09:09:32 +0200

    ca fonctionne

--- files/scripts/portfolioBuilder.js
+++ files/scripts/portfolioBuilder.js
@@ -4,6 +4,7 @@ let artistsArray = []
 let startX = 0;
 let endX = 0;
 let imgId = 0;
+let currentArtist = null;
 
 async function build() {
   let artists = await dataLoad.requestJSON(`/files/data/portfolio.json`);
@@ -21,10 +22,40 @@ async function build() {
     
   });
 
+  const carrouselButtonsLeft = document.querySelector('.carrouselL');
+  const carrouselButtonsRight = document.querySelector('.carrouselR');
+  const carrouselImgContainer = document.querySelector('.carrousel-img-container');
+
+  carrouselButtonsLeft.addEventListener('click', () => {
+    imgId == 0 ? imgId = currentArtist["oeuvres"].length - 1 : imgId -= 1;
+    setImgPortfolio(currentArtist);
+  });
+
+  carrouselButtonsRight.addEventListener('click', () => {
+    imgId == currentArtist["oeuvres"].length - 1 ? imgId = 0 : imgId += 1;
+    setImgPortfolio(currentArtist);
+  });
+
+  carrouselImgContainer.addEventListener('touchstart', (e) => {
+    startX = e.touches[0].clientX;
+  });
+
+  carrouselImgContainer.addEventListener('touchend', (e) => {
+    endX = e.changedTouches[0].clientX;
+    handleSwipe(currentArtist);
+  });
+
+  // Store current artist in a global variable
+  currentArtist = artistsArray[0];
+  loadArtist(currentArtist);
+
   loadArtist(artistsArray[0]);
 };
 
 function loadArtist(artist) {
+  currentArtist = artist;
+  imgId = 0;
+  
   const portfolioName = document.querySelector('#portfolio-name');
   const portfolioDescAutoportrait = document.querySelector('.portfolio-desc-autoportrait').children[0];
   const portfolioDescText = document.querySelector('.portfolio-desc-text').children[0];
@@ -55,16 +86,6 @@ function loadArtist(artist) {
     });
 	};
 
-  carrouselButtonsLeft.addEventListener('click', () => {
-  imgId == 0 ? imgId = artist["oeuvres"].length-1 : imgId -= 1;
-    setImgPortfolio(artist);
-  });
-
-  carrouselButtonsRight.addEventListener('click', () => {
-    imgId == artist["oeuvres"].length-1 ? imgId = 0 : imgId += 1;
-    setImgPortfolio(artist);
-  });
-
   setImgPortfolio(artist)
 
   // INFOS
@@ -73,15 +94,6 @@ function loadArtist(artist) {
   portfolioDescAutoportrait.setAttribute('src', `/files/assets/images/oeuvres/portfolios/${artist["folderName"]}/${artist["autoportrait"]}`);
 
   const carrouselImgContainer = document.querySelector('.carrousel-img-container');
-
-	carrouselImgContainer.addEventListener('touchstart', (e) => {
-		startX = e.touches[0].clientX;
-	});
-
-	carrouselImgContainer.addEventListener('touchend', (e) => {
-		endX = e.changedTouches[0].clientX;
-		handleSwipe(artist);
-	});
 };
 
 function handleSwipe(artist) {
@@ -101,7 +113,6 @@ function setImgPortfolio(artist) {
   const carrouselImgPortfolioTitle = document.querySelector('.carrousel-title').children[0];
   const carrouselImgPortfolio = document.querySelector('.carrousel-img-container').children[0];
   const previous = document.querySelector('.displayedImg');
-  imgId = 0;
 
   carrouselImgPortfolioTitle.innerHTML = artist["oeuvres"][imgId][0];
   carrouselImgPortfolio.setAttribute('src', `/files/assets/images/oeuvres/portfolios/${artist["folderName"]}/oeuvres/${artist["oeuvres"][imgId][1]}`);
